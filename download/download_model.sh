0e6b2d37fdc12ab0ccaed43f4dcf0c10a488aef5
Daniel-xsy <1491387884@qq.com>
2025-09-03 22:29:05 -0700

    Update script

--- download/download_model.sh
+++ download/download_model.sh
@@ -16,4 +16,6 @@ cd models/VOGUES/
 gdown 1exyOvOkueX2c-6e-oFr2YY38r3ImFCn8
 mkdir -p pretrained_models && cd pretrained_models 
 gdown 1fCNYPSlM06XQnDFCyo_qmuU72OzfUL3l
-cd ../../../
+cd ../ && mkdir -p detector/yolo/data && cd detector/yolo/data
+gdown 1mGpWL3fnQDDByNtj6eioHCsOt6uORYQb
+cd ../../../../
